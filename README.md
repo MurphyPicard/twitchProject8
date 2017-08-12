563182a5b57291a7caba104050c3898c15da6f23
Ara <ayaghsizian@gmail.com>
2017-08-12 13:47:11 -0400

    forgot to commit in a while - two ajax calls working - style needed

--- README.md
+++ README.md
@@ -1,1 +1,13 @@
 # Ara's Twitch API Project
+
+
+## Useful info:
+
+USAGE:
+Replace the Twitch API base URL https://api.twitch.tv/kraken with https://wind-bow.gomix.me/twitch-api. Use this endpoint according to the Twitch API documentation.
+
+NOTE:
+This server caches data to lower the request rate. To prevent abuses this server accepts GET requests only, and serves only routes /users/:user, /channels/:channel, and /streams/:stream. These are more than enough to complete the challenge.
+
+PEOPLE WHO REGULARLY STREAM:
+["ESL_SC2", "OgamingSC2", "cretetion", "freecodecamp", "storbeck", "habathcx", "RobotCaleb", "noobs2ninjas"]
---
+++ app.js
@@ -0,0 +1,55 @@
+
+
+
+var handlers = {
+
+  usernames: ["ESL_SC2", "OgamingSC2", "cretetion", "freecodecamp",
+              "storbeck", "habathcx", "RobotCaleb", "noobs2ninjas"],
+
+  counter: 1,
+
+
+  testIt: ()=>{
+    var url = 'https://wind-bow.gomix.me/twitch-api/';
+    // /users/:user, /channels/:channel, and /streams/:stream
+    handlers.usernames.forEach((name)=>{
+      $.ajax({
+        url: url + '/users/' + name,
+        success: (res)=>{
+            console.log("SUCCESS!!! This is /users/ for - " + name);
+            console.log(res);
+            $('.names').append(handlers.counter + ": " + res.display_name + '<br>');
+            handlers.counter++;
+        }//success
+      })//ajax
+      .then(function() {
+        $.ajax({
+          url: url + '/channels/' + name,
+          success: (res)=>{
+              console.log("SUCCESS!!! This is /channels/ for - " + name);
+              console.log(res);
+              $('.names').append(handlers.counter + ": " + "test channels <br>");
+              handlers.counter++;
+          }//success
+        })//ajax
+        .then(function() {
+           console.log("worked?");
+        }, function() {
+           console.log("didn't work");
+        });
+      })
+
+
+
+
+
+
+
+
+    });// forEach
+
+
+  }//testIt
+
+
+}
